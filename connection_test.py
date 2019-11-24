c7ec1c5aacac0fd8126eddbf86d93f9e309d362a
Max Castaneda <maxcastaneda@Maxs-MacBook-Pro.local>
2019-11-24 15:28:39 -0500

    WE GOIN WITH THE H2

--- connection_test.py
+++
@@ -1,6 +0,0 @@
-import mysql.connector
-
-cnx = mysql.connector.connect(user='teamdynamo', password='cop4710',
-                              host='34.74.3.25',
-                              database='beers')
-cnx.close()
